511edb8826b56279494566287073913703e56c74
mingi <mingi>
2018-10-02 20:25:57 +0900

    add trinity

---
+++ trinity/include/field.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#define u8 unsigned char
+
+u32 get_values(char* values, u8*** pdest, int size);
+void read_field_file(u8* field_file, u32* pfield_count);
+ 
+struct st_field** fields;
+
+struct st_field{
+  u32 start;
+  u32 size;
+  u32 type;
+  u32 value_count;
+  u32 marker_count;
+  u32 constraint_count;
+  u32 interest_count;
+  u64* values;
+  u8** markers;
+  u8** constraints;
+  u8** interests;
+};
+
