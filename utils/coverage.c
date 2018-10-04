e36245fd51dfadc1f08ffc436ddffb1785abb3a3
mingi <mingi>
2018-10-05 04:19:49 +0900

    add utils

---
+++ utils/coverage.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/prctl.h>
+#include <sys/resource.h>
+#include <string.h>
+
+int main(int argc, char** argv){
+  if(!strcmp(argv[1], "1"))
+    syscall(__NR_mmap, 0x2f002000, 0x1000, 3, 0x32, -1, 0);
+  if(!strcmp(argv[1], "0"))
+    syscall(__NR_mmap, 0x2f003000, 0x1000, 3, 0x32, -1, 0);
+
+  return 0;
+}
